1492a8e411c717af142961c3db8586eb007f0d42
Shawn Frank <shawn@mindhyve.com>
2021-02-16 09:12:54 +0400

    #1 - completed F1

---
+++ SampleProject/SampleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Shawn Frank on 16/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
